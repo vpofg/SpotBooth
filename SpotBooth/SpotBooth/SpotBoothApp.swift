3081f1623b00d29d7f7ed2ac937712cdb196f3cb
GacuGacu <91371291+GacuGacu@users.noreply.github.com>
2023-02-25 21:36:55 +0100

    Add files via upload

---
+++ SpotBooth/SpotBooth/SpotBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotBoothApp.swift
+//  SpotBooth
+//
+//  Created by Kacper Domagała on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpotBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
